db0abd6364518aaf14420d247257e695b2174dec
Marcos Souza Brito <njsudev5@hotmail.com>
2023-04-11 22:15:55 -0300

    criando a HomeView

---
+++ lib/app/views/home_view.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/src/widgets/framework.dart';
+
+class HomeView extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
