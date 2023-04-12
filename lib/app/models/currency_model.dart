9d41e2883b85732499ecd3c841833176ea74f779
Marcos Souza Brito <njsudev5@hotmail.com>
2023-04-11 21:09:29 -0300

    arrumando erros currency_model

--- lib/app/models/currency_model.dart
+++ lib/app/models/currency_model.dart
@@ -4,16 +4,14 @@ class CurrencyModel {
   final double dolar;
   final double euro;
 
-  CurrencyModel({this.name, this.real, this.dolar, this.euro});
+  CurrencyModel(
+      {this.name = '', this.real = 0.0, this.dolar = 0.0, this.euro = 0.0});
 
   static List<CurrencyModel> getCurrencies() {
     return <CurrencyModel>[
-      CurrencyModel(
-        name: 'Real', real: 1.0, dolar: 0.20, euro: 0.18),
-      CurrencyModel(
-        name: 'Dolar', real: 5.01, dolar: 1.0, euro: 0.92),
-      CurrencyModel(
-        name: 'Euro', real: 5.46, dolar: 1.09, euro: 1.0),
-    ]
+      CurrencyModel(name: 'Real', real: 1.0, dolar: 0.20, euro: 0.18),
+      CurrencyModel(name: 'Dolar', real: 5.01, dolar: 1.0, euro: 0.92),
+      CurrencyModel(name: 'Euro', real: 5.46, dolar: 1.09, euro: 1.0),
+    ];
   }
 }
